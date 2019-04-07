2fdfda203572a22c69097e9e32be09ba29de53e6
zubin | zeuscoder <zubin.pratap@gmail.com>
2019-04-07 21:18:01 +1000

    add README with details. Move logout button. Add boostrap and basic grid styling

--- README.md
+++ README.md
@@ -1,68 +1,30 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+#NOTES
+<hr>
+### General summary
+The app uses `react-dom-router` for routing and navigation. 
 
-## Available Scripts
+The App has the following 'static' page components:
+1) Home.js
+2) About.js
+3) NewPage.js
 
-In the project directory, you can run:
+The App has the following pages that are components that are dynamically rendered with data taken from App level state:
+1) Page.js  (this page takes in data from this.state.pages which is an array of page objects, and has one hard coded page for demo purposes)
 
-### `npm start`
+Dynamic page components <b>need login </b> to see the title and body of those pages.
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+### App level state
+```
+//App.js
+{
+    pages: [ array of objects containing page related data],
+    user: { object that has username, pwd, and loggedin boolean }
 
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
+}
+```
 
-### `npm test`
+### App handlers passed as props
+1) `updateUserState`: is passed as prop to the `<Login />` component in all `<Page />` components. Upon login the name and password properties are updated in the App-level `state.user` object.
 
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+2) `addPage`: handles the new page creation in `<NewPage />. Takes an object with the title and body properties and constructs an object that is added to the App-level `this.state.pages` array. This handler is passed to the `<Route />` that renders `<NewPage />`
 
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
--- src/components/App.js
+++ src/components/App.js
@@ -26,8 +26,7 @@ class App extends Component {
         name: "",
         pwd: "",
         loggedIn: false
-      },
-      currentPageIndex: 0
+      }
     };
   }
 
@@ -57,10 +56,10 @@ class App extends Component {
     });
   };
 
-  renderPagesInNav() {
+  renderUserPagesInNav() {
     return this.state.pages.map(page => {
       return (
-        <li key={page.content.title}>
+        <li key={page.content.title} className="nav-item">
           <Link to={`/pages/${page.path}`}>{page.content.title}</Link>
         </li>
       );
@@ -75,80 +74,96 @@ class App extends Component {
   render() {
     let loggedIn = this.state.user.loggedIn;
     return (
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            {this.renderPagesInNav()}
-            <li>
-              <Link to="/newpage"> Add A New Page</Link>
-            </li>
-          </ul>
+      <div className="container-fluid">
+        <Router>
+          <div className="row">
+            <div className="vertical-nav col-2">
+              <ul>
+                <li className="nav-item">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/about">About</Link>
+                </li>
+                {/* render <li> elements for each page in this.state.pages  */}
+                {this.renderUserPagesInNav()}
+                <li className="nav-item">
+                  <Link to="/newpage"> Add A New Page</Link>
+                </li>
 
-          <hr />
+                {/* LOGOUT */}
+                <li>
+                  <div className="logout">
+                    {loggedIn && (
+                      <Link to="/" onClick={this.handleLogout}>
+                        Logout
+                      </Link>
+                    )}
+                  </div>
+                </li>
+              </ul>
+            </div>
 
-          {/* ROUTES FOR STATIC PAGES */}
-          {/* HOME PAGE */}
-          <Route
-            exact
-            path="/"
-            render={props => (
-              <Home
-                {...props}
-                auth={loggedIn}
-                updateUserState={this.updateUserState}
+            <hr />
+
+            <div className="col-10">
+              {/* ROUTES FOR STATIC PAGES */}
+              {/* HOME PAGE */}
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Home
+                    {...props}
+                    auth={loggedIn}
+                    updateUserState={this.updateUserState}
+                  />
+                )}
               />
-            )}
-          />
 
-          {/* ABOUT PAGE */}
-          <Route
-            path="/about"
-            render={props => (
-              <About
-                {...props}
-                auth={loggedIn}
-                updateUserState={this.updateUserState}
+              {/* ABOUT PAGE */}
+              <Route
+                path="/about"
+                render={props => (
+                  <About
+                    {...props}
+                    auth={loggedIn}
+                    updateUserState={this.updateUserState}
+                  />
+                )}
               />
-            )}
-          />
 
-          {/* NEW PAGE CREATION */}
-          <Route
-            path="/newpage"
-            render={props => (
-              <NewPage {...props} auth={loggedIn} addPage={this.addPage} updateUserState={this.updateUserState}/>
-            )}
-          />
-
-          {/* ROUTES FOR USER PAGES */}
-          {/* TODO: use the context system to pass the updateUserState() function? */}
-          <Route
-            exact
-            path="/pages/:id"
-            render={props => (
-              <Page
-                {...props}
-                auth={loggedIn}
-                pages={this.state.pages}
-                updateUserState={this.updateUserState}
+              {/* NEW PAGE CREATION */}
+              <Route
+                path="/newpage"
+                render={props => (
+                  <NewPage
+                    {...props}
+                    auth={loggedIn}
+                    addPage={this.addPage}
+                    updateUserState={this.updateUserState}
+                  />
+                )}
               />
-            )}
-          />
-          <div>
-            {loggedIn && (
-              <Link to="/" onClick={this.handleLogout}>
-                Logout
-              </Link>
-            )}
+
+              {/* ROUTES FOR USER PAGES */}
+              {/* TODO: use the context system to pass the updateUserState() function? */}
+              <Route
+                exact
+                path="/pages/:id"
+                render={props => (
+                  <Page
+                    {...props}
+                    auth={loggedIn}
+                    pages={this.state.pages}
+                    updateUserState={this.updateUserState}
+                  />
+                )}
+              />
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </div>
     );
   }
 }
